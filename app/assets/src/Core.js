100840663ed548ebf549a665511785d70f1c6ccd
victor <victor-brambilla@hotmail.com>
2022-06-21 19:30:03 -0400

    feat(cep): Criado função para gerar um comentário com o cep digitado

--- app/assets/src/Core.js
+++ app/assets/src/Core.js
@@ -1,9 +1,58 @@
-const aNewFunction = () => {
-  // A content here
+const client = ZAFClient.init();
+
+let ticket_id;
+
+
+
+const getInfoTicket = async () => {
+  try {
+    const result = await client.get('ticket')
+    ticket_id = result.ticket.id
+  } catch (e) {
+    console.log(`Error request ${e}`);
+  }
+}
+
+const searchCEP = async () => {
+    const cep = document.getElementById("input-cep").value;
+    const button = document.getElementById("button-cep");
+    const loading = document.getElementById("loading");
+    button.disabled = true;
+    loading.style.display = "block";
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await response.json();
+    const { logradouro, bairro, localidade, uf, complemento } = data;
+    const body ={
+      ticket :{
+        comment:{
+          html_body: `<p style="color: #ff00000; font-weight: 500; color: #04363c; font-size: 1rem; text-align: center;">Endereço</p>
+          <p>${logradouro}, ${complemento}</p>
+          <p>${bairro}, ${localidade}/${uf}</p>
+          <p>${cep}</p>`
+        }
+      }
+    }
+
+    client.request({
+      method: 'PUT',
+      url: `/api/v2/tickets/${ticket_id}`,
+      contentType: 'application/json',
+      data: JSON.stringify(body),
+      secure: true
+    }).then(function (data) {
+      console.log("Ticket atualizado com sucesso");
+    }).catch(function (e) {
+      console.log(`Erro: ${e}`);
+    }).finally(function () {
+      button.disabled = false;
+      loading.style.display = "none";
+    });
 };
 
 const Core = {
-  aNewFunction,
+  searchCEP,
+  getInfoTicket,
+
 };
 
 export default Core;
--- app/assets/src/Main.js
+++ app/assets/src/Main.js
@@ -1,15 +1,31 @@
 import Core from "./Core.js";
 
 const client = ZAFClient.init();
-let settings;
 
 client.metadata().then((metadata) => {
-  settings = metadata.settings;
+
+  Core.getInfoTicket();
+});
+
+client.on('app.registered', function(e) {
+ document.getElementById("button-cep").addEventListener("click", Core.searchCEP);
 });
 
 const Main = async () => {
   const App = document.getElementById("app");
-  let appBody = `<div id="main-content"></div>`;
+  let appBody = `
+  <div id="main-content">
+    <h1>Insira um endereço</h1>
+
+    <section>
+      <input name="cep" id="input-cep" type="text" maxlength="8" placeholder="CEP"/>
+      <button id="button-cep">Inserir CEP</button>
+
+    </section>
+
+    <p style="display:none; text-align: center;" id="loading">Carregando...</p>
+
+  </div>`;
 
   // Write App
   App.innerHTML = appBody;
