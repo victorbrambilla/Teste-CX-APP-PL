b567dffcf82e6b76582b60a2e47bb0c039ccb7a2
victor <victor-brambilla@hotmail.com>
2022-06-21 20:13:13 -0400

    style(size): Altura 100%

--- app/assets/src/App.js
+++ app/assets/src/App.js
@@ -2,7 +2,7 @@
 let client = ZAFClient.init();
 
 client.on("app.registered", (e) => {
-  client.invoke("resize", { width: "100%", height: "130px" });
+  client.invoke("resize", { width: "100%", height: "100%" });
 });
 
 // Create screen context
